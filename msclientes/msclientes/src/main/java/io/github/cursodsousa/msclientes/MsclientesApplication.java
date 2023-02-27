46fd02f8ca0cdda5b65875f3afd2224f1b85a341
Roberto Soysa <robertokbs@gmail.com>
2023-02-27 17:31:31 -0300

    atualizando

--- msclientes/msclientes/src/main/java/io/github/cursodsousa/msclientes/MsclientesApplication.java
+++ msclientes/msclientes/src/main/java/io/github/cursodsousa/msclientes/MsclientesApplication.java
@@ -2,9 +2,11 @@ package io.github.cursodsousa.msclientes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class MsclientesApplication {
 
 	public static void main(String[] args) {
